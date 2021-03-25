884f1ccadb0bcf53f5b16b8409693dcae6d05624
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-03-25 16:47:20 +0000

    pipe7

---
+++ scripts/kubectl.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+#install-awscli
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+#install-eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+#install-kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl 
